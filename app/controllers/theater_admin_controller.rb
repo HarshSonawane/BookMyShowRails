b85e011ff0084da5801c895924b686ffc26c9fda
Harsh <sonawaneharshwardhan@gmail.com>
2021-06-30 13:50:08 +0530

    admin thi ngs added

--- app/controllers/theater_admin_controller.rb
+++ app/controllers/theater_admin_controller.rb
@@ -13,22 +13,54 @@ class TheaterAdminController < ApplicationController
   def shows
     @theater = UserTheater.where(user: current_user).first
     @shows = Show.joins(:screen).where(screen: { theater: @theater })
+    @show = Show.new
+    @movies = Movie.all
+    @screens = Screen.where(theater: @theater)
+  end
+
+  def update_screen
+    @screen = Screen.find params[:id]
+    @screen.update(screen_params)
+    redirect_to theater_admin_screens_path(current_user)
   end
 
   def screens
     @theater = UserTheater.where(user: current_user).first
     @screens = @theater.theater.screens
+    @screen = Screen.new
   end
 
   def create_screen
+    @screen = Screen.create(screen_params)
+    @theater = UserTheater.where(user: current_user).first
+    @screens = @theater.theater.screens
+    if @screen.save
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   def edit_screen
+    @theater = UserTheater.where(user: current_user).first
+    @screens = @theater.theater.screens
+    @screen = Screen.find params[:id]
     respond_to do |format|
       format.html
       format.js
     end
   end
+
+  def delete_screen
+    @screen = Screen.find params[:id]
+    @screen.delete
+    @theater = UserTheater.where(user: current_user).first
+    @screens = @theater.theater.screens
+    respond_to do |format|
+          format.js
+    end
+  end
+
 
   def create_show
   end
@@ -43,5 +75,6 @@ class TheaterAdminController < ApplicationController
   end
 
   def screen_params
+      params.require(:screen).permit(:name, :no_of_seats, :theater_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'theater#index'
   resources :theater_admin do
     get 'screens'
-    get 'edit_screen'
+    post 'create_screen'
     get 'shows'
+    post 'create_show'
   end
   resources :theater do
     get 'shows'
@@ -11,6 +12,9 @@ Rails.application.routes.draw do
 
   get '/movie/:id', to: 'theater#movie', as: 'movie'
   get '/show/:id', to: 'theater#movie_show', as: 'movie_show'
+  delete 'delete_screen/:id', to: 'theater_admin#delete_screen', as: 'delete_screen'
+  get 'edit_screen/:id', to: 'theater_admin#edit_screen', as: 'edit_screen'
+  patch 'update_screen/:id', to: 'theater_admin#update_screen', as: 'update_screen'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 end
